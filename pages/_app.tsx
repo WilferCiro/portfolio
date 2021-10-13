690ad68a97ea94f7b2b6472d5d8815a5470efd7f
wciro <wilcirom@gmail.com>
2021-10-12 20:00:59 -0500

    Optimizations images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,12 +11,112 @@ import MyHead from '@/components/MyHead'
 const Header = dynamic(() => import('@/components/Header'))
 const Footer = dynamic(() => import('@/components/Footer'))
 const FloatInfo = dynamic(() => import('@/components/FloatInfo'))
+
+import Particles from "react-tsparticles";
+
+
 import { theme } from '../styles/theme'
 import 'antd/dist/antd.dark.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const particlesInit = (main: any) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container: any) => {
+    console.log(container);
+  };
+
   return (
     <>
+      <div className="particles">
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#000",
+              },
+            },
+            fpsLimit: 60,
+            interactivity: {
+              detectsOn: "canvas",
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.2,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outMode: "bounce",
+                random: false,
+                speed: 1,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      </div>
       <MyHead />
       <Header />
       <FloatInfo />
@@ -42,6 +142,12 @@ function MyApp({ Component, pageProps }: AppProps) {
           }
           .main-content{
             min-height: 100vh;
+          }
+          .particles{
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            z-index: -1;
           }
         `}
       </style>
