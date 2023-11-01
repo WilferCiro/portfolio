18d29c87bfd64bd6a93b96cddb40da0d05b1cb7e
Wilfer Daniel Ciro Maya <wilcirom@gmail.com>
2023-11-01 14:18:37 -0500

    feature: add translations core

---
+++ src/data/i18n/ui.ts
@@ -0,0 +1,84 @@
+export const languages = {
+  es: "Español",
+  en: "English",
+};
+
+export const defaultLang = "es";
+export const showDefaultLang = false;
+
+export const ui = {
+  es: {
+    "header.tooltip_home": "Inicio",
+    "header.tooltip_about_me": "Sobre mi",
+    "header.tooltip_projects": "Proyectos",
+    "header.tooltip_theme": "Alternar tema",
+    "header.tooltip_lang": "Cambiar idioma",
+    "header.title_home": "Ir al inicio de la página",
+    "header.title_about_me": "Ir a mi descripción",
+    "header.title_projects": "Ver la página de proyectos",
+    "header.title_contact": "Ir a la página de contacto",
+
+    "contact.lets_build":
+      "Contáctame y construyamos juntos soluciones para el mundo.",
+    "contact.send_mail": "Envíame un correo",
+    "contact.send_whatsapp": "Envíame un whatsapp",
+    "contact.schedule": "Agenda una cita",
+
+    "projects.discover":
+      "Descubre todos los proyectos que he desarrollado a lo largo de mi carrera",
+    "projects.my_projects": "Mis proyectos",
+
+    "about.description_1":
+      "Como full stack developer comprometido, responsable y apasionado por la programación, tengo una amplia experiencia en el desarrollo de software y en la implementación de soluciones innovadoras. Me siento cómodo trabajando en diferentes lenguajes de programación y frameworks, así como en la creación de aplicaciones web, móviles y de escritorio.",
+    "about.description_2":
+      "He trabajado en equipos multidisciplinarios, colaborando en la creación de soluciones a medida para clientes de diferentes sectores. Soy capaz de entender los requerimientos y necesidades del cliente, y proponer soluciones que cumplan con sus expectativas. Además, cuento con habilidades en la gestión de proyectos y en la comunicación asertiva con los diferentes miembros del equipo.",
+
+    "hobbies.barista": "Barismo",
+    "hobbies.games": "Juegos",
+    "hobbies.cats": "Gatos",
+    "hobbies.read": "Leer",
+    "hobbies.robotic": "Robótica",
+    "hobbies.gym": "Gimnasio",
+    "hobbies.my_hobbies": "Mis pasatiempos",
+
+    "generic.fullstack_developer": "Desarrollador Fullstack",
+    "generic.contactme": "Contáctame",
+  },
+  en: {
+    "header.tooltip_home": "Home",
+    "header.tooltip_about_me": "About me",
+    "header.tooltip_projects": "Projects",
+    "header.tooltip_theme": "Toggle theme",
+    "header.tooltip_lang": "Change lang",
+    "header.title_home": "Go to home page",
+    "header.title_about_me": "Go to my description",
+    "header.title_projects": "Go to projects page",
+    "header.title_contact": "Go to contact page",
+
+    "contact.lets_build":
+      "Contact me and let's build solutions for the world together.",
+    "contact.send_mail": "Send me an Email",
+    "contact.send_whatsapp": "Send me a whatsapp",
+    "contact.schedule": "Schedule",
+
+    "projects.discover":
+      "Discover all the projects I have developed throughout my career",
+    "projects.my_projects": "My projects",
+
+    "about.description_1":
+      "As a committed, responsible full stack developer with a passion for programming, I have extensive experience in software development and the implementation of innovative solutions. I am comfortable working in different programming languages and frameworks, as well as creating web, mobile and desktop applications.",
+    "about.description_2":
+      "I have worked in multidisciplinary teams, collaborating in the creation of tailored solutions for clients from different sectors. I am able to understand the client's requirements and needs, and propose solutions that meet their expectations. In addition, I have skills in project management and assertive communication with different team members.",
+
+    "hobbies.barista": "Barista",
+    "hobbies.games": "Games",
+    "hobbies.cats": "Cats",
+    "hobbies.read": "Read",
+    "hobbies.robotic": "Robotics",
+    "hobbies.gym": "Gym",
+    "hobbies.my_hobbies": "My hobbies",
+
+    "generic.fullstack_developer": "Fullstack Developer",
+    "generic.contactme": "Contact me",
+  },
+} as const;
